97cedaa1a6a6c77366aeda360d6521f6c3e7d44d
seba85valle <seba85valle@gmail.com>
2025-06-09 19:31:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("menu-toggle");
   const menu = document.getElementById("navbarMenu");
 
+  // Toggle para mostrar/ocultar menú
   toggleButton.addEventListener("click", function () {
     menu.classList.toggle("show");
   });
-});
 
+  // Cerrar el menú al hacer clic en cualquier enlace del menú
+  const navLinks = menu.querySelectorAll("a");
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      if (menu.classList.contains("show")) {
+        menu.classList.remove("show");
+      }
+    });
+  });
+});
