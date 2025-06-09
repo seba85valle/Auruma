2b2e069042864d389bfbac5c3794a56d86e66560
seba85valle <seba85valle@gmail.com>
2025-06-09 18:53:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("menu-toggle");
+  const menu = document.getElementById("navbarMenu");
+
+  toggleButton.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
+});
 
